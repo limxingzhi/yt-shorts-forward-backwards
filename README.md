c99f094fea0630f99428c4c982448042ad8c4ef1
limxingzhi <limxz97@gmail.com>
2023-10-28 13:59:21 +0800

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# YouTube Shorts Forwards Backwards
+
+This is a simple chrome extension that controls YouTube Shorts to play forwards and backwards with left/right arrow keys.
+
---
+++ content.js
@@ -0,0 +1,19 @@
+function getShortsPlayer () {
+  const nodes = Array.from(document.querySelectorAll('video'))
+  if (nodes.length === 3) return nodes[1]
+  else if (nodes.length === 2) return nodes[0]
+  else return null
+}
+
+document.addEventListener('keydown', function(event) {
+  // checkf if URL is a shorts URL
+  if (!window.location.href.includes('youtube.com/shorts')) return
+
+  if (event.key === 'ArrowLeft') {
+    const player = getShortsPlayer()
+    if (player) player.currentTime -=3
+  } else if (event.key === 'ArrowRight') {
+    const player = getShortsPlayer()
+    if (player) player.currentTime +=3
+  }
+});
